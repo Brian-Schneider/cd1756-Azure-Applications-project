17455e82fb78ab6b3a0d245d0a02a269c78520e4
Brian Schneider de Lima <bschneiderlima@gmail.com>
2025-08-05 14:24:56 -0700

    Fix: importing werkzeug.middleware.proxy_fix

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,6 +7,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
